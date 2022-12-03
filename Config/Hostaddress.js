61a82777559b37c54978a0c5dfd65e1ba995f590
kamyab78 <amirhosseinghassemi78@yahoo.com>
2022-12-03 16:28:45 +0330

    Add production url

--- Config/Hostaddress.js
+++ Config/Hostaddress.js
@@ -1,7 +1,7 @@
 export const Config = () => {
     return {
         'baseUrl':'nakama-api.darkube.app',
-        // 'baseUrl':'nakama-api.darkube.app',
+        // 'baseUrl':'nakama-api.gerehverse.com',
         'gerehUrl':'https://stage-gereh.darkube.app/',
         "gerehUrlApi":"https://gereh-api.darkube.app/"
     }
